514bf8f7b5d816e020fdf707488a8886c06f608b
vikrantsrgc <145971065+vikrantsrgc@users.noreply.github.com>
2025-03-30 11:45:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function minDate(dates) {
 	}
 
 	const sorted = [...dates].sort()
-	console.log(sorted[0])
+	return sorted[0]
 }
 
 // Do not change the code
@@ -44,4 +44,4 @@ var dates = [
   "2023/03/30",
 ];
 
-// alert(minDate(dates));
+alert(minDate(dates));
